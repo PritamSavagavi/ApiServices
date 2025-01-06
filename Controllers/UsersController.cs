d7298a2f92d577bf7232db82482a4ae3835f48f6
PritamSavgavi <pritamsavagavi@gmail.com>
2025-01-06 17:51:39 +0530

    Building a walking skeleton part 1 completed

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,40 +1,28 @@
 using ApiServices.Data;
 using ApiServices.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 
 namespace ApiServices.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController(DataContext context) : ControllerBase
     {
-        private readonly DataContext _context;
-
-        public UsersController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            if (_context.Users is null) return NotFound();
-            return _context.Users.ToList();
+            if (context.Users is null) return NotFound();
+            return  await context.Users.ToListAsync();
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            //return _context.Users.FirstOrDefault(a => a.Id = id);
-            var user = _context.Users?.FirstOrDefault(u => u.Id == id);
-
-            if (user is null)
-            {
-                return NotFound(); // HTTP 404
-            }
-
-            return user;
+            var user = await context.Users.FindAsync(id);
+            if (user is null) return NotFound(); // HTTP 404
+            return Ok(user);
         }
     }
 }
